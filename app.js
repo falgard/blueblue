306afa42724791a1970ef55628731fcff981c116
falgard <mikael.falgard@gmail.com>
2018-02-18 12:56:00 +0100

    Init site commit

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
-var express     = require('express');
-var morgan      = require('morgan');
+const express = require('express');
+const morgan = require('morgan');
+const path = require('path');
 
 var app = express();
 var server = require('http').Server(app);
@@ -7,11 +8,14 @@ port = process.env.PORT || 8080;
 
 // Catch all other routes and return the index file
 app.get('/', (req, res) => {
-  res.send("Blå blå är kärleken - 28 Juli 2018");
+  // res.send("Blå blå är kärleken - 28 Juli 2018 / Cissi & Micke");
+  res.sendFile(path.join(__dirname + '/index.html'));
 });
 
 // use morgan to log requests to the console
 app.use(morgan('dev')); 
+
+app.use(express.static(path.join(__dirname + '/static')));
 
 server.listen(port);
 console.log('http://localhost:' + port);
