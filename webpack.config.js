476191d73bd449e1015307e6ef009a9547db2545
falgard <mikael.falgard@gmail.com>
2018-02-20 16:20:18 +0100

    setup react and webpack

---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+const path = require('path');
+const webpack = require('webpack');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin')
+
+let pathsToClean = [
+  'dist'
+]
+let cleanOptions = {
+  root: __dirname,
+  verbose: true
+}
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index.[chunkhash].js'
+  },
+  module: {
+      loaders: [
+        {
+            test: /\.js$/,
+            loader: 'babel-loader',
+            exclude: /node_modules/,
+            query: {
+              presets: ['es2015', 'react']
+            }
+        },
+        {
+          test: /\.scss$/,
+          loader: ExtractTextPlugin.extract('css-loader!sass-loader')
+      }
+    ]
+  },
+  plugins: [
+    new CleanWebpackPlugin(pathsToClean, cleanOptions),
+    new ExtractTextPlugin('dist/styles/main.css', {
+        allChunks: true
+    }),
+    new HtmlWebpackPlugin({
+      template: 'src/templates/index.ejs'
+    })
+  ]
+}
